96a9c74f758a9e40e68e598e470e7368466b2885
abhyudayarocks <s6hggsnywg@privaterelay.appleid.com>
2026-07-28 15:33:50 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,19 +1,42 @@
-const CACHE_NAME = 'sacred-jaap-v2'; // Increment this number for every future update
-const ASSETS = [
-  '/', '/index.html', '/manifest.json', '/imageedit_2_3478557917.jpg', 
-  '/brahma.jpeg', '/ganesh.jpeg', '/vishnu.jpeg', '/durga.jpg', '/shiva.jpeg', '/satyam.jpeg'
+const CACHE_NAME = 'sacred-jaap-v3';
+const URLS_TO_CACHE = [
+    '/',
+    '/index.html',
+    '/manifest.json',
+    '/imageedit_2_3478557917.jpg',
+    '/brahma.jpeg',
+    '/ganesh.jpeg',
+    '/vishnu.jpeg',
+    '/durga.jpg',
+    '/shiva.jpeg',
+    '/satyam.jpeg'
 ];
 
-self.addEventListener('install', e => {
-  self.skipWaiting();
-  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
+self.addEventListener('install', event => {
+    self.skipWaiting();
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(URLS_TO_CACHE))
+    );
 });
 
-self.addEventListener('activate', e => {
-  e.waitUntil(clients.claim());
-  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE_NAME ? caches.delete(k) : null))));
+self.addEventListener('activate', event => {
+    event.waitUntil(clients.claim());
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        fetch(event.request).catch(() => caches.match(event.request))
+    );
 });
